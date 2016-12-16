f698c1a90336b91c9ab2f1b52ccdb73d8b3bf532
Zachary Robbins <xaqiri@outlook.com>
2016-12-16 17:10:41 -0600

    Removed test quote

--- js/rqg.js
+++ js/rqg.js
@@ -6,13 +6,12 @@ $(document).ready(() => {
   let currAuthor = ''
   $('#generateQuote').on('click', () => {
     rand = Math.floor(Math.random() * (25))
-    $('#quote').html('Test quote')
-    /*$.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=25&callback=', a => {
+    $.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=25&callback=', a => {
       currQuote = $(a[rand].content).text()
       currAuthor = a[rand].title
       $('#quote').html(currQuote)
       $('#author').html(currAuthor)
-    })*/
+    })
   })
   $('#tweet').on('click', () => {
     $('a').attr('href', 'https://twitter.com/intent/tweet?text=' + currQuote + ' - ' + currAuthor)
